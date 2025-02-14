99ca6f038ce34ed43598c66eab9a9615696d3256
000ABDO <am9860316@gmail.com>
2025-02-14 22:16:52 +0200

    Saving local changes before deployment

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,28 @@
-import React from "react"; 
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "../../navbar";
 import Slider from "../../slider";
 import ProductsList from "../../productsList";
 import Create from "../../createproduct";
-import "./App.css"
-
+import "./App.css";
 
 export default function App() {
   return (
     <>
-      <Navbar /> {/* ✅ سيظهر في كل الصفحات */}
+      <Navbar />
 
       <Routes>
-        <Route path="/" element={ 
-          <> 
-            <Slider /> {/* ✅ يظهر فقط في الصفحة الرئيسية */}  
-            <ProductsList /> {/* ✅ يظهر فقط في الصفحة الرئيسية */}
-          </>
-        } />
+        <Route
+          path="/"
+          element={
+            <>
+              <Slider />
+              <ProductsList />
+            </>
+          }
+        />
         <Route path="/create" element={<Create />} />
       </Routes>
     </>
   );
 }
-
